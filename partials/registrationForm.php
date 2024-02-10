0c4b06033bebed1b52352fa68ad90dd316a22ec7
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-02-10 22:20:32 +0100

    correcao e modularizacao do projecto

---
+++ partials/registrationForm.php
@@ -0,0 +1,33 @@
+<div class="register-container" id="register">
+            <div class="top">
+                <span>Já tens uma conta? <a href="#" onclick="login()">Login</a></span>
+                <header>Inscreva-se</header>
+            </div>
+            <div class="two-forms">
+                <div class="input-box">
+                    <input type="text" class="input-field" placeholder="Digite o primeiro nome">
+                    <i class="bx bx-user"></i>
+                </div>
+                <div class="input-box">
+                    <input type="text" class="input-field" placeholder="Digite o ultimo nome">
+                    <i class="bx bx-user"></i>
+                </div>
+            </div>
+            <div class="input-box">
+                <input type="text" class="input-field" placeholder="Digite o seu email">
+                <i class="bx bx-envelope"></i>
+            </div>
+            <div class="input-box">
+                <input type="password" class="input-field" placeholder="Crie uma palavra-passe">
+                <i class="bx bx-lock-alt"></i>
+            </div>
+            <div class="input-box">
+                <input type="submit" class="submit" value="Registrar">
+            </div>
+            <div class="two-col">
+                <div class="two">
+                    <label><a href="#">Termos & condições</a></label>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ views/services.php
@@ -0,0 +1,11 @@
+<?php
+$title="Serviços";
+require_once '../partials/header.php';
+?>
+
+<!---------------------------------- Serviços ---------------------------------------->
+<div class="form-box"> 
+<h1>nossos serviços</h1>
+</div>
+
+<?php require_once '../partials/footer.php'?>
