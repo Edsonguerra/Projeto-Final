ea50dcac039cb024705ab1f52fd795fba47a3422
Edsonguerra <edsonguerra004@gmail.com>
2024-03-05 16:29:53 -0800

    Alterações no nome de consultas

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-const selectBtn = document.querySelector(".selecionar-botao"),
-    items = document.querySelector(".lista");
-
-
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
-})
