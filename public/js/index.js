a18c69e3674ea08b599d8accd43a36a041fd98fa
Edsonguerra <edsonguerra004@gmail.com>
2024-03-07 14:18:02 -0800

    Alterações

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,3 @@ selectBtn.addEventListener("click", () => {
     selectBtn.classList.toggle("open");
 })
 
-items.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked");
-    })
-})
