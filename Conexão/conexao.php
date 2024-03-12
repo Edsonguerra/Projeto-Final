b639d3cb79363b87726372bb55590ded65ec224d
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-03-12 12:13:49 +0100

    update conexao

--- Conexão/conexao.php
+++ Conexão/conexao.php
@@ -6,5 +6,5 @@ $bancodedados ="caddstro";
 
 $conecta = mysqli_connect($localhost,$user,$passw,$bancodedados);
 
-$sql= mysqli_query($conecta,"SELECT *FROM user");
+$sql= mysqli_query($conecta,"SELECT * FROM user");
 echo var_dump($sql);
