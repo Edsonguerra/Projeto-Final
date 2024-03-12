50952d63c54daa3cacb45933c201bedc6bc5edd6
SilvioAnton12 <152030199+SilvioAnton12@users.noreply.github.com>
2024-03-12 13:18:44 +0000

    conexao do banco de dados feito

---
+++ Conexao/conexao.php
@@ -0,0 +1,27 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$bancodedados ="caddstro";
+
+$conexao = mysqli_connect($host,$user,$password,$bancodedados);
+
+if($conexao->connect_error){
+    die("Erro na conexão".$conexao->connect_error);
+}else{
+    echo "<h1>Conexão Feita com sucesso!</h1>";
+}
+
+$sql="SELECT * FROM user";
+$usuarios=$conexao->query($sql);
+
+if ($usuarios->num_rows > 0) {
+    while ($row = $usuarios->fetch_assoc()) {
+        echo "nome:".$row["nome"]."<br>";
+        echo "email:".$row["email"]."<br>";
+        echo "senha:".$row["senha"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+    
---
+++ Conexao/conexao2.php
@@ -0,0 +1,16 @@
+<?php
+$localhost="localhost";
+$user="root"
+$passw="";
+$bancodedados="cddstro";
+global $pdo;
+
+//orientada a objecto
+$pdo=new PDO("MSQL:dbname=".$bancodedados.";host=".$localhost,$user,$passw) ;
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$SQL=$pdo->query("SELECT*FROM user");
+$sql->execute();
+
+echo $sql->rowcount();   
+ 
