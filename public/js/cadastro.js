13727abfb4475297ff7b4b9ae5116cd30f6b2b31
Lourencolu <163518986+Lourencolu@users.noreply.github.com>
2024-03-15 13:03:20 +0100

    Add files via upload

---
+++ public/js/cadastro.js
@@ -0,0 +1,9 @@
+function validarEmail(email) {
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
+
+  function mascaraTelefone(telefone) {
+    var regex = /\d{2}-\d{5}-\d{4}/;
+    return regex.test(telefone);
+  }
