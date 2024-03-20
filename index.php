add4cfcbfe3d3d437810f657ef1040eba0e5b11a
Edsonguerra <edsonguerra004@gmail.com>
2024-03-20 09:17:15 -0800

    Estou fazendo o select das consultas

--- index.php
+++ index.php
@@ -1,37 +1,33 @@
 <?php
-// Conectar ao banco de dados (substitua pelas credenciais do seu banco)
-$conn = mysqli_connect("localhost", "seu_usuario", "sua_senha", "seu_banco");
+$servername = "localhost";
+$username = "seu_usuario";
+$password = "sua_senha";
+$dbname = "cadastro";
 
-// Consulta SQL para buscar consultas
-$sql = "SELECT id, nome FROM consultas";
-$result = mysqli_query($conn, $sql);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Verificar se a consulta foi bem-sucedida
-if (mysqli_num_rows($result) > 0) {
-  echo "<div class='Selecionar'>";
-  echo "  <div class='selecionar-botao'>";
-  echo "    <span class='texto'>Selecionar Consulta</span>";
-  echo "    <span class='down-arrow'>";
-  echo "      <i class='fa-solid fa-chevron-down'></i>";
-  echo "    </span>";
-  echo "  </div>";
-  echo "  <ul class='lista-consulta'>";
-  while ($row = mysqli_fetch_assoc($result)) {
-    $consultaId = $row["id"];
-    $consultaNome = $row["nome"];
-    echo "    <li class='lista'>";
-    echo "      <img class='img' width='35' src='public/assets/css/img/43493.png' alt=''>";
-    echo "      <span class='checked'>";
-    echo "        <i class='fa-solid fa-check check-icon'></i>";
-    echo "      </span>";
-    echo "      <span class='primeiro-lista'>" . $consultaNome . "</span>"; // Insere o nome da consulta dinamicamente
-    echo "    </li>";
-  }
-  echo "  </ul>";
-  echo "</div>";
-} else {
-  echo "Erro ao buscar consultas do banco de dados.";
+if (!$conn) {
+  die("Falha na conexão: " . mysqli_connect_error());
 }
 
-mysqli_close($conn);
+if (isset($_GET['consulta'])) {
+  $consulta = $_GET['consulta'];
+
+  $sql = "SELECT * FROM consultas WHERE nome = '$consulta'";
+
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+
+    echo "Consulta selecionada: " . $row['Consulta de Dermatologia'];
+    echo "Consulta selecionada: " . $row['Consulta de Pediatria'];
+    echo "Consulta selecionada: " . $row['Consulta de Hematologia'];
+    echo "Consulta selecionada: " . $row['Consulta de Genecologia'];
+    echo "Consulta selecionada: " . $row['Consulta de Estomatologia'];
+  
+  } else {
+    echo "Consulta não encontrada.";
+  }
+}
 ?>
