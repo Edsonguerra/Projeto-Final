c96b91b0ede14d660bf31620281d1ae17b00b9f5
SilvioAnton12 <152030199+SilvioAnton12@users.noreply.github.com>
2024-03-26 15:20:01 +0000

    alteraçao do create

--- Conexao/create2.php
+++ Conexao/create2.php
@@ -1,25 +1,24 @@
 <?php
 //conexao
-require_once 'bancodedados.caddstro';
-if(isset)($_POST['btn-registrar']):
+            
+require_once 'conexao.php';
+
+if(isset($_POST['btn-registrar'])){
     $nome = mysqli_escape_string($connect, $_POST['nome']);
     $sobrenome = mysqli_escape_string($connect,$_POST['sobrenome']);
     $email = mysqli_escape_string($connect, $_POST['email']);
     $senha = mysqli_escape_string($connect, $_POST['senha']);
 
-    $mysqli = new mysqli("localhost","user","password",);
+    $mysqli = new mysqli("localhost","user","password","database");
     $stmt = $msqli ->prepare ("INSERT INTO tabela(nome,email,idade) VALUES(?,?,?)");
     $stmt->bind_param("sss",$nome ,$email,$idade);
     $stmt->execute();
     $stmt->close();
-    ?
-
     
+    header("Location:registrationForm.php");
 
+}
 
-    if(mysqli_query)($connect,$sql));
-    header('Location:registrationForm.php?sucesso');
-else:
-    header('Location:registrationForm.php?erro');
-endif;
+?>
+    
     
