f6812a2c771fefc1a01a0f44604cb8abf3e2f610
Edsonguerra <edsonguerra004@gmail.com>
2024-04-04 23:45:18 -0800

    alterações

--- public/login.php
+++ public/login.php
@@ -1,21 +1,15 @@
 <?php
 if (isset($_POST['email']) || isset($_POST['senha'])) {
-    if (strlen($_POST['nome']) == 0) {
-        echo "Digite o seu nome";
-    } else if (strlen($_POST['ultimo_nome']) == 0) {
-        echo "Digite seu ultimo nome";
-    } else if (strlen($_POST['email']) == 0) {
+    if (strlen($_POST['email']) == 0) {
         echo "Digite o seu email";
     } else if (strlen($_POST['senha']) == 0) {
         echo "Digite a sua palavra-passe";
     } else {
 
-        $nome = $mysqli->real_escape_string($_POST['nome']);
-        $ultimonome = $mysqli->real_escape_string($_POST['ultimo_nome']);
         $email = $mysqli->real_escape_string($_POST['email']);
-        $password = $mysqli->real_escape_string($_POST['password']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuarios WHERE nome = '$nome' AND ultimo_nome = '$ultimo_nome' AND email = '$email' AND senha = '$senha'";
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
         $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
 
         $quantidade = $sql_query->num_rows;
