b32fa1e19c94fe54385068db995908fc069d2466
Edsonguerra <edsonguerra004@gmail.com>
2024-04-04 23:59:04 -0800

    alterações

--- public/login.php
+++ public/login.php
@@ -4,6 +4,13 @@ $user="root";
 $password="";
 $bancodedados ="site_marcação_de_consulta";
 
+$mysqli = new mysqli($host, $user, $password, $bancodedados);
+
+if ($mysqli->connect_errno) {
+    echo "Falha ao conectar ao MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
 if (isset($_POST['email']) || isset($_POST['senha'])) {
     if (strlen($_POST['email']) == 0) {
         echo "Digite o seu email";
@@ -11,8 +18,13 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
         echo "Digite a sua palavra-passe";
     } else {
 
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
+        if ($mysqli && $mysqli->connect_errno === 0) {
+            $email = $mysqli->real_escape_string($_POST['email']);
+            $senha = $mysqli->real_escape_string($_POST['senha']);
+        } else {
+            echo "Falha ao conectar ao MySQL";
+            exit();
+        }
 
         $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
         $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
