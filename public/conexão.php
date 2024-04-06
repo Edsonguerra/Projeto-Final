ba7e2eaf59ca9c215c6637813c3937e860c07420
Edsonguerra <edsonguerra004@gmail.com>
2024-04-05 21:27:10 -0800

    alterações

--- public/conexão.php
+++ public/conexão.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $password="";
-$bancodedados ="site_marcação_de_consult";
+$bancodedados ="site_marcação_de_consulta";
 
 $mysqli = new  mysqli($host, $user, $password, $bancodedados);
 
