f596243dac88f6b89511215c0786aa21b999f765
Edsonguerra <edsonguerra004@gmail.com>
2024-04-05 21:23:13 -0800

    ALTERAÇÕES

--- public/conexão.php
+++ public/conexão.php
@@ -2,12 +2,13 @@
 $host="localhost";
 $user="root";
 $password="";
-$bancodedados ="site_marcação_de_consulta";
+$bancodedados ="site_marcação_de_consult";
 
 $mysqli = new  mysqli($host, $user, $password, $bancodedados);
 
 if($mysqli->error) {
     die("Falha ao conectar ao banco de dados: " . $mysql->error );
 }
+
 
 ?>
