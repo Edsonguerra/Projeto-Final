4a48b0343b971238a0ad9aadc771f67dab5a333c
Edsonguerra <edsonguerra004@gmail.com>
2024-04-07 13:28:42 -0800

    alterações

--- public/login.php
+++ public/login.php
@@ -1,38 +1,54 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$bancodedados = "site_marcação_de_consulta";
+$host="localhost";
+$user="root";
+$password="";
+$bancodedados ="site_marcação_de_consulta";
 
-if (isset($_POST['email']) && isset($_POST['senha'])) {
-    $mysqli = new mysqli($host, $user, $password, $bancodedados);
-    if ($mysqli->connect_errno) {
-        echo "Falha ao conectar ao MySQL: " . $mysqli->connect_error;
-        exit();
-    }
+$mysqli = new mysqli($host, $user, $password, $bancodedados);
 
-    $email = $mysqli->real_escape_string($_POST['email']);
-    $senha = $mysqli->real_escape_string($_POST['senha']);
+if ($mysqli->connect_errno) {
+    echo "Falha ao conectar ao MySQL: " . $mysqli->connect_error;
+    exit();
+}
 
-    $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+if (isset($_POST['email']) || isset($_POST['senha'])) {
+    if (strlen($_POST['email']) == 0) {
+        echo "Digite o seu email";
+    } else if (strlen($_POST['senha']) == 0) {
+        echo "Digite a sua palavra-passe";
+    } else {
 
-    $quantidade = $sql_query->num_rows;
-
-    if ($quantidade == 1) {
-        $usuarios = $sql_query->fetch_assoc();
-
-        if (!isset($_SESSION)) {
-            session_start();
+        if ($mysqli && $mysqli->connect_errno === 0) {
+            $email = $mysqli->real_escape_string($_POST['email']);
+            $senha = $mysqli->real_escape_string($_POST['senha']);
+        } else {
+            echo "Falha ao conectar ao MySQL";
+            exit();
         }
 
-        $_SESSION['id'] = $usuarios['id'];
-        $_SESSION['nome'] = $usuarios['nome'];
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
 
-        header("Location: painel.php");
-        exit();
-    } else {
-        echo "<script>exibirMensagemErro('Falha ao entrar! E-mail ou Senha incorretos');</script>";
-    }
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+
+            $usuarios = $sql_query->fetch_assoc(); 
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuarios['id'];
+            $_SESSION['nome'] = $usuarios['nome'];
+
+            header("Location: painel.php");
+
+        } else {
+            echo "Falha ao entrar! E-mail ou Senha incorretos";
+        }
+    }   
+
 }
+
 ?>
