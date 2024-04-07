74c84eaeb764f18f8a48788650d82589e73e77e4
Edsonguerra <edsonguerra004@gmail.com>
2024-04-06 17:20:08 -0800

    alterações

--- processa_consulta.php
+++ processa_consulta.php
@@ -5,43 +5,40 @@ $password = "";
 $bancodedados = "site_marcação_de_consulta";
 
 
-// Conectar ao banco de dados
+
 $conexao = mysqli_connect($host, $user, $password, $bancodedados) or die("Erro ao conectar ao banco de dados!");
 
-// Extrair o tipo de consulta selecionado
-$tipoConsulta = $_POST['Consulta_']; // Substitua "_" pelo nome específico da consulta (ex: Consulta_Dermatologia)
 
-// Tratar a consulta de acordo com o tipo selecionado
+$tipoConsulta = $_POST['Consulta_de_Dermatologia']; 
+
+
 switch ($tipoConsulta) {
-  case "1": // Consulta de Dermatologia
-    // Código específico para agendar consulta de dermatologia
-    // ...
+  case "1": 
+
+
     break;
-  case "2": // Consulta de Pediatria
-    // Código específico para agendar consulta de pediatria
-    // ...
+  case "2": 
+ 
+
     break;
-  case "3": // Consulta de Hematologia
-    // Código específico para agendar consulta de hematologia
-    // ...
+  case "3": 
+
     break;
-  case "4": // Consulta de Ginecologia
-    // Código específico para agendar consulta de ginecologia
-    // ...
+  case "4":
+
     break;
-  case "5": // Consulta de Estomatologia
-    // Código específico para agendar consulta de estomatologia
-    // ...
+  case "5": 
+
     break;
 }
 
-// Fechar a conexão com o banco de dados
+
 mysqli_close($conexao);
 
-// Exibir mensagem de sucesso para o usuário
+
 echo "<p>Consulta agendada com sucesso!</p>";
 
-// Redirecionar o usuário para uma página de confirmação
+
 
 
 ?>
