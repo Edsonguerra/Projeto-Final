dc96ad3c0bb7e274285a1dce707804e6328c4c6d
Edsonguerra <edsonguerra004@gmail.com>
2024-04-06 16:10:05 -0800

    alterações

--- processa_consulta.php
+++ processa_consulta.php
@@ -6,22 +6,10 @@ $dbname = "consultas";
 
 include_once('conexao.php');
 
-
+  if(isset($_POST['submit']))
+  
   $conn = new mysqli($host, $user, $password, $bancodedados);
 
-  // Verifica se uma consulta foi selecionada
-  if (isset($_POST['consulta']) && !empty($_POST['consulta'])) {
-  $consultaSelecionada = $_POST['consulta'];
-
-  // Simula o processamento da seleção (substitua pela sua lógica real)
-  echo "Consulta selecionada: " . $consultaSelecionada;
-  echo "<br>Você pode prosseguir para marcar a consulta.";
-
-  // Você pode potencialmente redirecionar para outra página para detalhes da marcação
-  // echo "<br><a href='agendar_consulta.php'>Marcar Consulta</a>";
-} else {
-  echo "Nenhuma consulta selecionada.";
-}
 
 
 ?>
