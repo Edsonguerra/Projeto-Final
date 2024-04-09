8f6c96069ad1732a7e7ba6f166a9b97c78c2948a
Edsonguerra <edsonguerra004@gmail.com>
2024-04-09 15:22:47 -0800

    testando

--- painel.php
+++ painel.php
@@ -18,7 +18,20 @@ include('protect.php');
     <link href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,100..900;1,100..900&family=Glegoo:wght@400;700&family=M+PLUS+1+Code:wght@100..700&family=Quattrocento:wght@400;700&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+
+    <header>
+        <h1 class="Hospital">Hospital dos cajueiros</h1>
+            <nav class="icones">
+                <a href="Sobre.html">Sobre</a>
+                <a href="Sobre.html">Ajuda</a>
+                <a href="index.html">
+                    <a href="index.html">
+                        <button class="botaologin">Login</button>
+                    </a>
+                </a>
+            </nav>  
+    </header>
+
     <div class="boas-vindas">
         Bem vindo ao painel, <span class="nome-usuario"><?php echo $_SESSION['nome']; ?></span>
         <p>
