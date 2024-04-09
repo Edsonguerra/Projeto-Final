8e803ef2e7c50bf4c68e10ffe5ca3f65489b1875
Edsonguerra <edsonguerra004@gmail.com>
2024-04-09 14:42:25 -0800

    H

--- painel.php
+++ painel.php
@@ -9,7 +9,13 @@ include('protect.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,100..900;1,100..900&family=Glegoo:wght@400;700&family=M+PLUS+1+Code:wght@100..700&family=Quattrocento:wght@400;700&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -17,8 +23,19 @@ include('protect.php');
             <p>
                 <a href="logout.php">Sair</a>
             </p>
-
+                <h1 class="Hospital">Hospital dos cajueiros</h1>
+                    <nav class="icones">
+                        <a href="Sobre.html">Sobre</a>
+                        <a href="Sobre.html">Ajuda</a>
+                        <a href="index.html">
+                            <a href="index.html">
+                                <button class="botaologin">Login</button>
+                            </a>
+                        </a>
+                    </nav> 
     </header>
+
+
 
 </body>
 </html>
