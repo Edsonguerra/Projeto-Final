81bd2030716836bef84eb73bb08b6a81c4c802d4
Edsonguerra <edsonguerra004@gmail.com>
2024-04-10 08:33:00 -0800

    t

---
+++ painel.php
@@ -0,0 +1,36 @@
+<?php
+
+include('protect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/assets/css/painel.style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,100..900;1,100..900&family=Glegoo:wght@400;700&family=M+PLUS+1+Code:wght@100..700&family=Quattrocento:wght@400;700&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+
+  
+
+
+
+</body>
+
+<script>
+    let btn = document.querySelector('#btn')
+    let menu = document.querySelector('.menu')
+
+    btn.onclick = function () {
+        menu.classList.toggle('active')
+    };
+</script>
+</html>
