89c0bb5b39ebfbc38a268970a18875a745ac255e
Edsonguerra <edsonguerra004@gmail.com>
2024-04-09 18:29:07 -0800

    TESTANDO

--- painel.php
+++ painel.php
@@ -28,7 +28,7 @@ include('protect.php');
             <li>
                 <a href="#" class="logo">
                     <img src="public/assets/css/img/43493.png" alt="">
-                    <span class="nav-item">H.Cajueiros</span>
+                    <span class="nav-item">MENU</span>
                 </a>
             </li>
             <li><a href="#">
