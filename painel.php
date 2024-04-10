b8b6fd819c809c2d757f74fac6e7f6d9d01e813c
Edsonguerra <edsonguerra004@gmail.com>
2024-04-10 09:52:30 -0800

    k

--- painel.php
+++ painel.php
@@ -51,7 +51,7 @@ include('protect.php');
             </li>
 
             <li>
-                <a href="logout.php">
+                <a href="logout.php" class="sair">
                     <i class="meu-icone fa-solid fa-right-from-bracket"></i>
                     <span class="nav-item">Sair</span>
                 </a>
