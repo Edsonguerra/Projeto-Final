e65bd4aca0f5c640ec3c71e51481755e44ddd574
Edsonguerra <edsonguerra004@gmail.com>
2024-04-17 12:27:57 -0800

    pagina oficial

--- painel.php
+++ painel.php
@@ -9,7 +9,7 @@ include('protect.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public/assets/css/painell.style.css">
+    <link rel="stylesheet" href="./public/assets/css/painnell.style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Encode+Sans+Condensed:wght@100;200;300;400;500;600;700;800;900&family=Gudea:ital,wght@0,400;0,700;1,400&family=Inter:wght@100..900&family=Jura:wght@300..700&family=Noto+Sans+Mono:wght@100..900&family=PT+Mono&family=Sawarabi+Gothic&family=Teko:wght@300..700&display=swap" rel="stylesheet">
@@ -117,6 +117,11 @@ include('protect.php');
         }, 2000); // 1 segundo
     }
     });
+
+
+
+<!------------------------------------- Consultas -------------------------------------------->
+
     </script>
 
     <?php
@@ -155,6 +160,7 @@ include('protect.php');
             </ul>   
         </div>    
     </form>
+
 
 
     <script src="public/js/index.js"></script> 
