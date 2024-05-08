12aa063369aed190f05e4e88f6efbf21b302f2ce
Edsonguerra <edsonguerra004@gmail.com>
2024-05-07 16:08:23 -0800

    alterações

--- views/Criar_consulta.php
+++ views/Criar_consulta.php
@@ -1,15 +1,17 @@
-<?php include('../modules/conexao.php');
+<?php
+include('../modules/conexao.php'); 
 
-if(isset($_POST ['submit'])){
-    $nome_da_consulta=$_POST['nome_da_consulta'];
-    $area_profissional=$_POST['area_profissional'];
+if (isset($_POST['submit'])) {
+    $nome = $_POST['nome'];
+    $tipo_consulta = $_POST['tipo_consulta']; 
 
-    $sql="insert into `tipos_de_consultas` (nome_da_consulta,area_profissional)values('$nome_da_consulta', '$area_profissional')";
-    $result=mysqli_query($mysqli,$sql);
-    if($result){
-        echo "Data inserted sucesso";
-    }else{
-        die($mysql->error($mysqli));
+    $sql = "INSERT INTO `consulta` (nome, tipo_consulta) VALUES ('$nome', '$tipo_consulta')";
+    $result = mysqli_query($mysqli, $sql);
+
+    if ($result) {
+        echo "Dados inseridos com sucesso!";
+    } else {
+        echo "Erro ao inserir dados: " . mysqli_error($mysqli);
     }
 }
 ?>
@@ -28,26 +30,23 @@ if(isset($_POST ['submit'])){
                 <img src="../public/assets/css/img/imagem do formulario.jpg" alt="">
             </div>
             <div class="submite-formulario">
-                
                 <a href="Gerenciamento_de_consultas.php">
                     <button class="voltar">Voltar</button>
                 </a>
-                
                 <form method="POST">
                     <h5 class="titulo">Criar Consultas</h5>
 
-
                     <div class="input-box">
-                        <input type="text" name="nome_da_consulta" class="input_nome" required placeholder="Digite o nome da consulta">
-                        <label for="nome completo" class="nome_da_consulta">Nome da consulta</label>
+                        <input type="text" name="nome" class="input_nome" required placeholder="Digite o nome da consulta">
+                        <label for="nome" class="nome_da_consulta">Nome da consulta</label>
                     </div>
 
                     <div class="input-box">
-                        <input type="text" name="area_profissional" class="input_area" required placeholder="Digite a área profissional">
+                        <input type="text" name="tipo_consulta" class="input_area" required placeholder="Digite a área profissional">
                         <label class="area_profissional">Área Profissional</label>
                     </div>
 
-                    <input type="submit" name="submit" id="submit" class="btn_enviar" value="Criar Consulta"> 
+                    <input type="submit" name="submit" id="submit" class="btn_enviar" value="Criar Consulta">
                 </form>
             </div>
         </div>
