8c1dd37b15ad504085bb652c94fd21f6a0cc5ce2
Edsonguerra <edsonguerra004@gmail.com>
2024-05-09 15:32:28 -0800

    alterações

--- modules/Eliminar_admin.php
+++ modules/Eliminar_admin.php
@@ -3,14 +3,14 @@
 include('../modules/conexao.php');
 
 if (isset($_GET['deleteid'])) {
-    $id_user = intval($_GET['deleteid']); // Converter para inteiro por segurança
+    $id_user = intval($_GET['deleteid']); 
 
-    $sql = "DELETE FROM `user` WHERE id_user = ?"; // Use o marcador de instrução preparada
+    $sql = "DELETE FROM `user` WHERE id_user = ?"; 
 
-    $stmt = mysqli_prepare($mysqli, $sql); // Prepara a instrução
+    $stmt = mysqli_prepare($mysqli, $sql); 
 
     if ($stmt) {
-        mysqli_stmt_bind_param($stmt, "i", $id_user); // Vincular o parâmetro
+        mysqli_stmt_bind_param($stmt, "i", $id_user); 
         if (mysqli_stmt_execute($stmt)) {
             header('location:../views/Administradores.php');
         } 
