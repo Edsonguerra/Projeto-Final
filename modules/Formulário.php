182892f948379cd7cd304e39f7bd6e9522288cf8
Edsonguerra <edsonguerra004@gmail.com>
2024-05-13 17:29:24 -0800

    alterações

--- modules/Formulário.php
+++ modules/Formulário.php
@@ -10,5 +10,5 @@ include_once('conexao.php');
 
         $result =   mysqli_query($mysqli, "INSERT INTO paciente (nome_completo,sexo,data_de_nascimento,historico_medico) VALUES ('$nome_completo', '$sexo', '$data_de_nascimento', '$historico_medico')");
     }
-    header("Location: ../views/Formulario_de_consulta.php");
+    header("Location: ../views/Consulta.php");
 ?>
