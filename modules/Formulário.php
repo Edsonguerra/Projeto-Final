0fb60244affb6c146463c7ceea4868082408ccb0
Edsonguerra <edsonguerra004@gmail.com>
2024-06-01 09:02:40 -0800

    alterações

--- modules/Formulário.php
+++ modules/Formulário.php
@@ -1,17 +1,30 @@
 <?php
 include_once('conexao.php');
-    if(isset($_POST['submit']))
-    {
 
-        $nome_completo = $_POST['nome_completo'];
-        $sexo = $_POST['sexo'];
-        $data_de_nascimento = $_POST['data_de_nascimento'];
-        // echo $_SESSION;
-        // die();
-        // $user_id=$_SESSION['id_user'];
 
-        $result =   mysqli_query($mysqli, "INSERT INTO paciente (nome_completo,sexo,data_de_nascimento) VALUES ('$nome_completo', '$sexo', '$data_de_nascimento')");
-        header("Location: ../views/Consulta.php");
-    }
+session_start();
+
+if (isset($_POST['submit'])) {
+
+    $nome_completo = $_POST['nome_completo'];
+    $sexo = $_POST['sexo'];
+    $data_de_nascimento = $_POST['data_de_nascimento'];
+
+
+    if (isset($_SESSION['id_user'])) {
+        $id_user = $_SESSION['id_user'];
+
   
+        $result = mysqli_query($mysqli, "INSERT INTO paciente (nome_completo, sexo, data_de_nascimento, user_id_user) VALUES ('$nome_completo', '$sexo', '$data_de_nascimento', '$id_user')");
+
+        if ($result) {
+            echo "Paciente inserido com sucesso!";
+        } else {
+            echo "Erro ao inserir paciente: " . mysqli_error($mysqli);
+        }
+    } else {
+        echo "Erro: usuário não está logado.";
+    }
+    header("Location: ../views/Consulta.php"); 
+}
 ?>
