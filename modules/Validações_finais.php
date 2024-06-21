71fda1e7eeaacfc8daad5a0fe68c19bcfdccffdf
Edsonguerra <edsonguerra004@gmail.com>
2024-06-21 14:20:04 -0800

    alterações

--- modules/Validações_finais.php
+++ modules/Validações_finais.php
@@ -2,6 +2,10 @@
 include('../modules/conexao.php');
 include('../modules/protect.php'); 
 
+$consulta_nome = htmlspecialchars($_GET['consulta_nome'] ?? '');
+$nome_completo = htmlspecialchars($_GET['nome_completo'] ?? '');
+$data_de_nascimento = htmlspecialchars($_GET['data_de_nascimento'] ?? '');
+$sexo = htmlspecialchars($_GET['sexo'] ?? '');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +20,7 @@ include('../modules/protect.php');
     <div class="consultas-container">
         <select name="id" class="input" id="consultasSelecionadas">
         </select>
-        <label class="titulo_consulta">Consulta/as selecionada/as</label>
+        <label class="titulo_consulta">Consulta selecionada: <?= $consulta_nome ?></label>
     </div>
 
     <div class="Formulario">
@@ -31,24 +35,24 @@ include('../modules/protect.php');
                 <form action="../modules/Validações_finais.php" method="POST">
                     <h5 class="titulo">Validações Finais</h5>
                     <div class="input-box">
-                        <input type="text" name="nome_completo" id="nome_completo" class="input_nome" value="<?= htmlspecialchars($_GET['nome_completo'] ?? '') ?>" readonly>
+                        <input type="text" name="nome_completo" id="nome_completo" class="input_nome" value="<?= $nome_completo ?>" readonly>
                         <label for="nome_completo" class="nome_completo">Nome completo do Paciente</label>
                     </div>
                     <div class="input-box">
-                        <input type="text" name="data_de_nascimento" id="data_de_nascimento" class="input-bilhete" value="<?= htmlspecialchars($_GET['data_de_nascimento'] ?? '') ?>" readonly>
+                        <input type="text" name="data_de_nascimento" id="data_de_nascimento" class="input-bilhete" value="<?= $data_de_nascimento ?>" readonly>
                         <label for="data_de_nascimento" class="bilhete">Data de nascimento</label>
                     </div>
                     
                     <br>
                     <div class="Genero">
                         <br><br>
-                    <p class="Sexo">Sexo:</p>
-                        <input type="radio" id="feminino" name="sexo" value="Feminino" <?= (isset($_GET['sexo']) && $_GET['sexo'] == 'Feminino') ? 'checked' : '' ?> required>
+                        <p class="Sexo">Sexo:</p>
+                        <input type="radio" id="feminino" name="sexo" value="Feminino" <?= ($sexo == 'Feminino') ? 'checked' : '' ?> required>
                         <label for="feminino">Feminino</label>
                     </div>
     
                     <div class="Genero">
-                        <input type="radio" id="Masculino" name="sexo" value="Masculino" <?= (isset($_GET['sexo']) && $_GET['sexo'] == 'Masculino') ? 'checked' : '' ?> required>
+                        <input type="radio" id="Masculino" name="sexo" value="Masculino" <?= ($sexo == 'Masculino') ? 'checked' : '' ?> required>
                         <label for="Masculino">Masculino</label>
                     </div>
 
@@ -56,7 +60,6 @@ include('../modules/protect.php');
                         <label class="data_nascimento" for="data_nascimento">Data e hora da consulta:</label> 
                         <input type="datetime-local" name="data" id="date" class="inputUser" required>
                     </div>
-
 
                     <div class="input-box">
                         <input type="text" name="estado" id="nestado" class="input_estado" placeholder="Digite o estado da consulta" required>
