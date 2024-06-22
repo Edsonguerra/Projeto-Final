0c9c9226114ee4ac6e228f14678ea5ccd4ff4dc2
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-06-22 13:13:02 -0400

    add the doctor approaching

--- views/Gestão.php
+++ views/Gestão.php
@@ -6,15 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/assets/css/Área administrativa.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <title>Document</title>
+    <title>Dashboard</title>
 </head>
 <body>
     <section id="sidebar">
 
         <a href="#" class="brand" data-text="hospital"> <i class="fa-solid fa-hospital icon"></i> Hospital Cajueiros</a>
         <ul class="side-menu">
+        <?php if(true):?>
+        <li class="divider" data-text="doctores">Doctores</li>
+        
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-hospital-user icon"></i>Atender Consultas</i> 
+                </a>
+                </li>
+        <?php endif;?>
+        
+        <?php if(true):?>
 
-            <li class="divider" data-text="doctores">Doctores</li>
+            <li class="divider" data-text="doctores">Secretário Clínico</li>
 
 
             <li>
@@ -40,8 +51,7 @@
             <li><a href="Area.php"><i class="fa-solid fa-list-ol icon"></i>Área</a></li>
             <li><a href="Administradores.php"> <i class="fa-solid fa-users icon"></i>Adminstradores</a></li>
                  
-                
-
+            <?php endif;?>
             <li class="divider"  data-text="Voltar ou sair">Voltar ou sair</li>
             <li><a href="painel.php"><i class="fa-solid fa-arrow-left-long icon"></i>Voltar</a></li>
             <li><a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket icon"></i>Sair</a></li>        
