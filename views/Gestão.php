57a237ef25df799b38b1bfe8bd84d9afdcd3cd20
Edsonguerra <edsonguerra004@gmail.com>
2024-06-25 10:27:42 -0800

    alterações

--- views/Gestão.php
+++ views/Gestão.php
@@ -10,51 +10,50 @@
 </head>
 <body>
     <section id="sidebar">
-
         <a href="#" class="brand" data-text="hospital"> <i class="fa-solid fa-hospital icon"></i> Hospital Cajueiros</a>
         <ul class="side-menu">
-        <?php if($_SESSION['doctor']):?>
-        <li class="divider" data-text="doctores">Doctores</li>
-        
-        <li>
-            <a href="Atender_consultas.php">
-                <i class="fa-solid fa-hospital-user icon"></i>Atender Consultas</i><i class="fa-solid fa-chevron-right icon-rigth"></i> 
+            <?php if(isset($_SESSION['doctor']) && $_SESSION['doctor']): ?>
+            <li class="divider" data-text="doctores">Doctores</li>
+            <li>
+                <a href="Atender_consultas.php">
+                    <i class="fa-solid fa-hospital-user icon"></i>Atender Consultas<i class="fa-solid fa-chevron-right icon-rigth"></i> 
                 </a>
-                </li>
-        <?php endif;?>
-        
-        <?php if($_SESSION['funcionario']&&$_SESSION['administrador']):?>
+            </li>
+            <?php endif; ?>
+            
+            <?php if(isset($_SESSION['funcionario']) && $_SESSION['funcionario'] && isset($_SESSION['administrador']) && $_SESSION['administrador']): ?>
             <li class="divider" data-text="doctores">Secretário Clínico</li>
+            <a href="doctores.php">
+                    <i class="fa-solid fa-hospital-user icon"></i>Doctores</i> 
+            </a>
             <li>
                 <a href="#">
-                    <i class="fa-solid fa-hospital-user icon"></i>Dados & Validações <i class="fa-solid fa-chevron-right icon-rigth"></i> 
+                    <i class="fa-solid fa-hospital-user icon"></i>Dados & Validações<i class="fa-solid fa-chevron-right icon-rigth"></i> 
                 </a>
-                <?php $areas= mysqli_query($mysqli, "SELECT * FROM area"); ?>
-                    <ul class="side-dropdown">
-      <?php if($areas):?>
-        <?php while($area_data = mysqli_fetch_assoc($areas)):?>
-            <li><a href="Consulta_validação.php?areaId=<?=$area_data['id']?>"><?=$area_data['nome']?></a></li>
-            <?php endwhile; ?>            
-        <?php endif;?>
-                    </ul>
-            </li>            
-            <li class="divider"  data-text="criar">Consultas</li>
+                <?php $areas = mysqli_query($mysqli, "SELECT * FROM area"); ?>
+                <ul class="side-dropdown">
+                    <?php if($areas): ?>
+                        <?php while($area_data = mysqli_fetch_assoc($areas)): ?>
+                            <li><a href="Consulta_validação.php?areaId=<?= $area_data['id'] ?>"><?= $area_data['nome'] ?></a></li>
+                        <?php endwhile; ?>            
+                    <?php endif; ?>
+                </ul>
+            </li>   
+
+            <li class="divider" data-text="criar">Consultas</li>
             <li><a href="Gerenciamento_de_consultas.php"><i class="fa-solid fa-notes-medical icon"></i>Gerenciamento de Consultas</a></li>
-
-
-            <li class="divider"  data-text="área administrativa">Área Administrativa</li>
+            <li class="divider" data-text="área administrativa">Área Administrativa</li>
             <li><a href="Area.php"><i class="fa-solid fa-list-ol icon"></i>Área</a></li>
-            <li><a href="Administradores.php"> <i class="fa-solid fa-users icon"></i>Adminstradores</a></li>
-                 
-            <?php endif;?>
-            <li class="divider"  data-text="Voltar ou sair">Voltar ou sair</li>
+            <li><a href="Administradores.php"><i class="fa-solid fa-users icon"></i>Adminstradores</a></li>
+            <?php endif; ?>
+            <li class="divider" data-text="Voltar ou sair">Voltar ou sair</li>
             <li><a href="painel.php"><i class="fa-solid fa-arrow-left-long icon"></i>Voltar</a></li>
             <li><a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket icon"></i>Sair</a></li>        
         </ul>
     </section>
-        <div class="barra">A</div>
-        <label class="Admin" >Bem vindo/a</label>
-        <label class="admin2" >A área administrativa</label>
+    <div class="barra">A</div>
+    <label class="Admin">Bem vindo/a</label>
+    <label class="admin2">A área administrativa</label>
     
     <script src="../public/js/Gestão.js"></script> 
 </body>
--- views/painel.php
+++ views/painel.php
@@ -24,7 +24,7 @@
                     <span class="nav-item">Ajuda</span>
                 </a>
             </li>
-            <?php if($_SESSION['doctor'] || $_SESSION['funcionario']): ?>
+            <?php if(isset($_SESSION['doctor']) || isset($_SESSION['funcionario'])): ?>
             <li>
                 <a href="Gestão.php">
                     <i class="fa-solid fa-users"></i>
@@ -58,71 +58,69 @@
         <?php
             $consulta = mysqli_query($mysqli, "SELECT * FROM consulta"); 
         ?>
-<div style="margin-top:80px; display: flex; justify-content:center;gap:20px;">
-    <?php if(isset($_SESSION['doctor']) || (isset($_SESSION['funcionario']) && isset($_SESSION['administrador']))):?>
-        <div class="Selecionar">
-        <p >A marcação de Consulta esta Exclusiva Somente aos Pacientes.</p>
-        <br>
-        <p>Cadastra-te como paciente e marque a tua consulta</p>
-    </div>
-    <?php else :?>
-    <div class="areaContainer">
-        <select  name="hospitalArea" id="hospitalArea">
-            <option value="area">Selecionar Área</option>
-        <?php $areas= mysqli_query($mysqli, "SELECT * FROM area"); ?>
-          <?php if($areas):?>
-            <?php while($area_data = mysqli_fetch_assoc($areas)):?>
-                <option name=<?=$area_data['nome']?> value=<?=$area_data['id']?>> <?=$area_data['nome']?> </option>
-                <?php endwhile; ?>            
-            <?php endif;?>
-        </select>
-        </div>
+        <div style="margin-top:80px; display: flex; justify-content:center;gap:20px;">
+            <?php if(isset($_SESSION['doctor']) || (isset($_SESSION['funcionario']) && isset($_SESSION['administrador']))):?>
+                <div class="Selecionar">
+                    <p>A marcação de Consulta está Exclusiva Somente aos Pacientes.</p>
+                    <br>
+                    <p>Cadastra-te como paciente e marque a tua consulta</p>
+                </div>
+            <?php else :?>
+                <div class="areaContainer">
+                    <select name="hospitalArea" id="hospitalArea">
+                        <option value="area">Selecionar Área</option>
+                        <?php $areas = mysqli_query($mysqli, "SELECT * FROM area"); ?>
+                        <?php if($areas): ?>
+                            <?php while($area_data = mysqli_fetch_assoc($areas)): ?>
+                                <option name="<?= $area_data['nome'] ?>" value="<?= $area_data['id'] ?>"> <?= $area_data['nome'] ?> </option>
+                            <?php endwhile; ?>            
+                        <?php endif; ?>
+                    </select>
+                </div>
     
-    <form action="../modules/Formulário_de_consulta.php" method="POST" onsubmit="return validarFormulario()">
-    <div class="Selecionar" id="selecionarConsulta">
-                    <div class="selecionar-botao">
-                        <span class="texto">Selecionar Consulta</span>
-                        <span class="down-arrow">
-                            <i class="fa-solid fa-chevron-down"></i>
-                        </span> 
-                    </div>
+                <form action="../modules/Formulário_de_consulta.php" method="POST" onsubmit="return validarFormulario()">
+                    <div class="Selecionar" id="selecionarConsulta">
+                        <div class="selecionar-botao">
+                            <span class="texto">Selecionar Consulta</span>
+                            <span class="down-arrow">
+                                <i class="fa-solid fa-chevron-down"></i>
+                            </span> 
+                        </div>
                         <ul class="lista-consulta" id="lista-consultas">
-                        <li class="lista">
+                            <li class="lista">
                                 <span class="primeiro-lista">Nenhuma Área selecionada</span>
                             </li>
                         </ul>        
-                </div>    
-            </form>
-            
-    <?php endif; ?>
+                    </div>    
+                </form>
+            <?php endif; ?>
         </div>
         <script>
             function validarFormulario(){
                 if(document.querySelectorAll(".consulta-checkbox")){
                     var checkboxes = document.querySelectorAll(".consulta-checkbox");
 
-var peloMenosUmSelecionado = false;
+                    var peloMenosUmSelecionado = false;
 
-for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-        peloMenosUmSelecionado = true;
-        break;
-    }
-}
+                    for (var i = 0; i < checkboxes.length; i++) {
+                        if (checkboxes[i].checked) {
+                            peloMenosUmSelecionado = true;
+                            break;
+                        }
+                    }
 
-if (!peloMenosUmSelecionado) {
-    alert("Por favor, selecione pelo menos uma consulta.");
-    return false;
-}
+                    if (!peloMenosUmSelecionado) {
+                        alert("Por favor, selecione pelo menos uma consulta.");
+                        return false;
+                    }
 
-return true;
-}
-
-document.getElementById('marcaConsultaBtn').addEventListener('click', function() {
-document.getElementById('selecionarConsulta').scrollIntoView({ behavior: 'smooth' });
-});
+                    return true;
                 }
-              
+
+                document.getElementById('marcaConsultaBtn').addEventListener('click', function() {
+                    document.getElementById('selecionarConsulta').scrollIntoView({ behavior: 'smooth' });
+                });
+            }
         </script>
 
         <div class="contudo-abaixo">
@@ -138,70 +136,71 @@ document.getElementById('selecionarConsulta').scrollIntoView({ behavior: 'smooth
     <script>
         let btn = document.querySelector('#btn');
         let sidebar = document.querySelector('.sidebar');
-        let hospitalArea=document.querySelector("#hospitalArea");
+        let hospitalArea = document.querySelector("#hospitalArea");
 
         function liFunction(element){
-        element.classList.toggle("checked");
-        const checkbox =element.querySelector(".consulta-checkbox");
-        checkbox.checked = !checkbox.checked;
+            element.classList.toggle("checked");
+            const checkbox = element.querySelector(".consulta-checkbox");
+            checkbox.checked = !checkbox.checked;
 
-        if(localStorage.getItem("consultasObjectos")){
-            const consultasObject=JSON.parse(localStorage.getItem("consultasObjectos"));
-            if(consultasObject[checkbox.name]){
-                delete consultasObject[checkbox.name];
+            if(localStorage.getItem("consultasObjectos")){
+                const consultasObject = JSON.parse(localStorage.getItem("consultasObjectos"));
+                if(consultasObject[checkbox.name]){
+                    delete consultasObject[checkbox.name];
+                }else{
+                    consultasObject[checkbox.name] = checkbox.value;
+                }
+                localStorage.setItem("consultasObjectos", JSON.stringify(consultasObject));              
             }else{
-                consultasObject[checkbox.name]=checkbox.value;
+                const obj = {};
+                obj[checkbox.name] = checkbox.value;
+                localStorage.setItem("consultasObjectos", JSON.stringify(obj));  
             }
-            localStorage.setItem("consultasObjectos",JSON.stringify(consultasObject));              
-        }else{
-            const obj={};
-            obj[checkbox.name]=checkbox.value;
-            localStorage.setItem("consultasObjectos",JSON.stringify(obj));  
-        }
 
-        let checkedItems = document.querySelectorAll(".lista input[type='checkbox']:checked"),
-            btnText = document.querySelector(".texto");
-        if (checkedItems.length > 0) {
-            console.log("Selecionado");
-        } else {
-            console.log("Não selecionado");
-        } 
-    };
+            let checkedItems = document.querySelectorAll(".lista input[type='checkbox']:checked"),
+                btnText = document.querySelector(".texto");
+            if (checkedItems.length > 0) {
+                console.log("Selecionado");
+            } else {
+                console.log("Não selecionado");
+            } 
+        };
 
         btn.onclick = function () {
             sidebar.classList.toggle('active');
         };
 
-        hospitalArea.addEventListener("change",async (el)=>{
+        hospitalArea.addEventListener("change", async (el) => {
             if(localStorage.getItem("consultasObjectos")){
                 localStorage.clear("consultasObjectos");
             }
 
-            if(el.target.value!=="area"){
-                localStorage.setItem("areaId",el.target.value);
-                const result= await fetch(`http://localhost/Projeto-Final/modules/consultasApi.php/?idArea=${el.target.value}`);
+            if(el.target.value !== "area"){
+                localStorage.setItem("areaId", el.target.value);
+                const result = await fetch(`http://localhost/Projeto-Final/modules/consultasApi.php/?idArea=${el.target.value}`);
                 const data = await result.json();     
-                const dados_da_consulta=document.querySelector("#lista-consultas");
-                dados_da_consulta.innerHTML='';
+                const dados_da_consulta = document.querySelector("#lista-consultas");
+                dados_da_consulta.innerHTML = '';
 
-                data.forEach(element =>{
-                    dados_da_consulta.innerHTML+=`
-                   <li class="lista" style="z-index:200;" onclick="liFunction(this)" >
+                data.forEach(element => {
+                    dados_da_consulta.innerHTML += `
+                        <li class="lista" style="z-index:200;" onclick="liFunction(this)">
                             <img class="img" width="35" src="../public/assets/css/img/43493.png" alt="">
                             <span class="checked"><i class="fa-solid fa-check check-icon"></i></span>
                             <span class="primeiro-lista">${element.nome}</span>
-                            <input type="checkbox" name=${element.nome}  value=${element.id_da_consulta} class="consulta-checkbox">
+                            <input type="checkbox" name=${element.nome} value=${element.id_da_consulta} class="consulta-checkbox">
                         </li>
-                `});
-                dados_da_consulta.innerHTML+=`<button class="button" type="submit">Marcar Consulta</button>`;
+                    `;
+                });
+                dados_da_consulta.innerHTML += `<button class="button" type="submit">Marcar Consulta</button>`;
             }else{
                 localStorage.clear("areaId");
-                const dados_da_consulta=document.querySelector("#lista-consultas");
-                dados_da_consulta.innerHTML=`
-                   <li class="lista">
-                            <span class="primeiro-lista">Nenhuma Área selecionada</span>
-                        </li>
-                `
+                const dados_da_consulta = document.querySelector("#lista-consultas");
+                dados_da_consulta.innerHTML = `
+                    <li class="lista">
+                        <span class="primeiro-lista">Nenhuma Área selecionada</span>
+                    </li>
+                `;
             }
         });
     </script>
