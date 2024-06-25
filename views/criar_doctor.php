a4312115b5874153b2041cb66805c8864a52922c
Edsonguerra <edsonguerra004@gmail.com>
2024-06-25 14:37:08 -0800

    alterações

--- views/criar_doctor.php
+++ views/criar_doctor.php
@@ -67,7 +67,7 @@ if ($result_funcionarios) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/assets/css/doctores.css">
+    <link rel="stylesheet" href="../public/assets/css/doctoress.css">
     <title>Document</title>
 </head>
 <body>
