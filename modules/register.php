4a19389b98db434b4d6f2f1090834629c0a572c6
Edsonguerra <edsonguerra004@gmail.com>
2024-06-25 22:20:39 -0800

    alterações

--- modules/register.php
+++ modules/register.php
@@ -1,28 +1,19 @@
 <?php
 include_once('conexao.php');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nome = $_POST['nome'];
-    $ultimo_nome = $_POST['ultimo_nome'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+$nome = $_POST['nome'];
+$ultimo_nome = $_POST['ultimo_nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-    // Hashing da senha antes de armazenar no banco de dados
-    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+$result = mysqli_query($mysqli, "INSERT INTO user(nome,ultimo_nome,email,senha) VALUES ('$nome','$ultimo_nome','$email','$senha')");
 
-    // Inserção no banco de dados
-    $result = mysqli_query($mysqli, "INSERT INTO user (nome, ultimo_nome, email, senha) VALUES ('$nome', '$ultimo_nome', '$email', '$senha_hash')");
+if ($result) {
 
-    if ($result) {
-        $mensagemCadastro = "Cadastro realizado com sucesso.";
-    } else {
-        $mensagemCadastro = "Erro ao cadastrar usuário.";
-    }
+$mensagemCadastro = "";
+} 
+session_start();
+$_SESSION['cadastro_msg'] = $mensagemCadastro;
 
-    session_start();
-    $_SESSION['cadastro_msg'] = $mensagemCadastro;
-
-    header("Location: ../views/login.php");
-    exit();
-}
+header("Location: ../views/login.php");
 ?>
