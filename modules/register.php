f48c47faf27e6f860fd75a9a01362b97759d421d
Edsonguerra <edsonguerra004@gmail.com>
2024-06-27 13:40:05 -0800

    alterações

--- modules/register.php
+++ modules/register.php
@@ -4,7 +4,8 @@ include_once('conexao.php');
 $nome = $_POST['nome'];
 $ultimo_nome = $_POST['ultimo_nome'];
 $email = $_POST['email'];
-$senha = $_POST['senha'];
+$senha = md5($_POST['senha']);
+
 
 $result = mysqli_query($mysqli, "INSERT INTO user(nome,ultimo_nome,email,senha) VALUES ('$nome','$ultimo_nome','$email','$senha')");
 
